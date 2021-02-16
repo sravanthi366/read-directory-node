2e12ae182d8a9be462793401407d25af0d0cbb28
sravanthi366 <sravanthimca95@gmail.com>
2021-02-16 09:55:00 +0530

    ready to deploy-v1.3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const directory = 'C:\\Users\\SRAVANTHI\\Documents\\GUVI_PRACTICE\\GUVI_LEARNING\\guvi_learning';
 const path = require('path');
 const fs = require('fs');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 
 const app = express()
 
